3cef405b99d5e326017b8e1997baaad8678f8e5e
Anouar Mahmoud Bellakha <ambellakha@sqli.com>
2019-06-06 02:12:10 +0000

    Create the storybook npm script

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from "@storybook/react"; // eslint-disable-line import/no-extraneous-dependencies
 
 function loadStories() {
-  const req = require.context("../stories", true, /\.stories\.js$/);
+  const req = require.context("../stories", true, /.jsx$/);
   req.keys().forEach((filename) => req(filename));
 }
 
