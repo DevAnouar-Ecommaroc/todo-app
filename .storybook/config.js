4099851612a44e3489f6765ef496e9565380cbc3
Anouar Mahmoud Bellakha <ambellakha@sqli.com>
2019-06-06 20:55:59 +0000

    Register the storybooks's accessibility addon, and configure it globally

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,8 +2,10 @@
 
 import { configure, addDecorator } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
 
 addDecorator(withKnobs);
+addDecorator(withA11y);
 
 function loadStories() {
   const req = require.context("../stories", true, /.jsx$/);
